8d7d690fa8300853223c8bde6b56a3c85cbe951f
yuuki1967 <yuki06.endo@gmail.com>
2022-03-18 10:17:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.post("/update", function(req, res, next){
 });
 
 app.get("/search/:name", function(req, res, next){
-  var name = mongoSanitize.sanitize(req.params.name); 
-  // var name = req.params.name;
+  // var name = mongoSanitize.sanitize(req.params.name); 
+  var name = req.params.name;
   var query1 = {username:name};
   Message.find(query1, function(err, msgs){
     if(err) throw err;
